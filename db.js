900bfcab015012fa3a793ec2346c714be4f6b190
lucamuller2004 <lucamuller2004@gmail.com>
2025-09-29 07:10:59 +0000

    correct env variable name

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import { createClient } from "@libsql/client";
 
 const db = createClient({
   url: process.env.TURSO_URL,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  authToken: process.env.TURSO_TOKEN,
 });
 
 export default db;
