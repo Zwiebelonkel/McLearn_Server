54c70d94f6a597cf04547aff1972c2c11d39a93a
Luca <lucamuller2004@gmail.com>
2026-01-21 15:14:45 +0100

    Add JWT_SECRET and CORS configuration constants

--- config/constants.js
+++ config/constants.js
@@ -1,2 +1,9 @@
+export const JWT_SECRET = process.env.JWT_SECRET || "fallback-secret";
+
 export const boxIntervals = [0, 1, 3, 7, 16, 35];
 
+export const corsConfig = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-API-Key"],
+};
