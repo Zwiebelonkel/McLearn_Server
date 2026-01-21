f20f797ae707a6c60d2a744a8681e669ec81d21f
Luca <lucamuller2004@gmail.com>
2026-01-21 10:51:29 +0100

    Implement optional authentication middleware

--- middleware/optionalAuth.js
+++ middleware/optionalAuth.js
@@ -1,1 +1,11 @@
+import jwt from "jsonwebtoken";
 
+export const optionalAuth = (req, _res, next) => {
+  const header = req.headers.authorization;
+  if (header?.startsWith("Bearer ")) {
+    try {
+      req.user = jwt.verify(header.split(" ")[1], process.env.JWT_SECRET);
+    } catch {}
+  }
+  next();
+};
